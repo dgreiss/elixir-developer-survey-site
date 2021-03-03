d5a66407c4cb5db77224d2e88bd522df3d133920
David Greiss <david.dgreiss@gmail.com>
2021-03-03 08:22:57 -0500

    Add url to releases

--- config/releases.exs
+++ config/releases.exs
@@ -28,7 +28,9 @@ config :ex_survey, ExSurveyWeb.Endpoint,
     port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
-  secret_key_base: secret_key_base
+  secret_key_base: secret_key_base,
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  server: true
 
 # ## Using releases (Elixir v1.9+)
 #
